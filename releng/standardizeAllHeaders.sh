f231fc1e1c1fc20954f6362bdfd83c5a94c55610
Mikio Takeuchi <mikio.takeuchi@gmail.com>
2016-03-13 00:28:02 +0900

    standardize header

---
+++ releng/standardizeAllHeaders.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Dave Grove
+
+mydir="`dirname "$0"`"
+top="`cd "$mydir"/../.. && pwd`"
+cdir="`pwd`"
+[ "$cdir" = "/" ] && cdir="$cdir."
+cd "$top"
+
+# NOTE: x10.common/src to avoid including x10.common/contrib
+
+for srcdir in \
+	xasdi
+do
+  echo "Standardizing file headers in $srcdir"
+  cd $srcdir
+  JAVA_FILES=`find . -name .git -prune -o -name "*.java" -print`
+  X10_FILES=`find . -name .git -prune -o -name "*.x10" -print`
+  for file in $JAVA_FILES $X10_FILES
+  do
+      hasHeader=yes
+      `grep -q "This file is part of the XASDI project (https://github.com/x10-lang/xasdi)." $file` || hasHeader=no
+      `grep -q "This file is licensed to You under the Eclipse Public License (EPL)" $file` || hasHeader=no
+      `grep -q "(C) Copyright" $file` || hasHeader=no
+      if [[ "X$hasHeader" == "Xno" ]]; then
+	  echo "Invoking standardizeHeader.pl on $file"
+	  $top/xasdi/releng/standardizeHeader.pl $file
+      fi
+  done
+  cd $top
+done
